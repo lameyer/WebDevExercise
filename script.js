9b92872f145cdb4c6be554f1a2e40da7feeeff63
Lindsey <lameyer@users.noreply.github.com>
2015-05-21 00:45:48 -0700

    add popover

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
   $("#purple").hover(function(){
     $(".link").css("color", "#E066FF");
-  }, function() {
+  }, function(){
     $('.link').css("color", "");
+  });
+
+  $(".popover").hide();
+
+  $(".trigger").hover(function(){
+    $(".popover").show();
+  }, function(){
+    $(".popover").hide();
   });
 });
