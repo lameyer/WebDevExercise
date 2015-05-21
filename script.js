76532e1a94883d5f2895082657129d320ac1d2ce
Lindsey <lameyer@users.noreply.github.com>
2015-05-21 00:53:21 -0700

    prettify

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
+
+  // make all other links purple when hover over purple link
   $("#purple").hover(function(){
     $(".link").css("color", "#E066FF");
   }, function(){
     $('.link').css("color", "");
   });
 
+  // hide popover upon load
   $(".popover").hide();
 
+  // show popover on hover over trigger
   $(".trigger").hover(function(){
     $(".popover").show();
   }, function(){
     $(".popover").hide();
   });
+
 });
