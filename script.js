37fd4f3c9513f501728e0313197ab01294732bd7
Lindsey <lameyer@users.noreply.github.com>
2015-06-01 18:55:13 -0700

    some more clean up

--- script.js
+++ script.js
@@ -7,26 +7,9 @@ $(document).ready(function(){
     $(".link").css("color", "");
   });
 
+  // buttons!!
   $(":button").click(function(){
     alert("Ah! You clicked me!");
   });
-
-//   // hide popover upon load
-//   $(".popover").hide();
-
-//   // show popover on hover over trigger
-//   $(".trigger").hover(function(){
-//     $(".popover").show();
-//     // popover stays up when mouse over it
-//     $(".popover").on("mouseenter", function(){
-//       $(this).show();
-//     });
-//     // popover hides when mouse out of it
-//     $(".popover").on("mouseleave", function(){
-//       $(this).hide();
-//     });
-//   }, function(){
-//     $(".popover").hide();
-//   });
 
 });
