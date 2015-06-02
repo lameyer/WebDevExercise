9ac085b806c22b16d82c0ffa11c381e2801fcb30
Lindsey <lameyer@users.noreply.github.com>
2015-06-01 17:21:24 -0700

    simplify popover

--- script.js
+++ script.js
@@ -4,25 +4,29 @@ $(document).ready(function(){
   $("#purple").hover(function(){
     $(".link").css("color", "#E066FF");
   }, function(){
-    $('.link').css("color", "");
+    $(".link").css("color", "");
   });
 
-  // hide popover upon load
-  $(".popover").hide();
-
-  // show popover on hover over trigger
-  $(".trigger").hover(function(){
-    $(".popover").show();
-    // popover stays up when mouse over it
-    $(".popover").on("mouseenter", function(){
-      $(this).show();
-    });
-    // popover hides when mouse out of it
-    $(".popover").on("mouseleave", function(){
-      $(this).hide();
-    });
-  }, function(){
-    $(".popover").hide();
+  $(":button").click(function(){
+    alert("Ah! You clicked me!");
   });
+
+//   // hide popover upon load
+//   $(".popover").hide();
+
+//   // show popover on hover over trigger
+//   $(".trigger").hover(function(){
+//     $(".popover").show();
+//     // popover stays up when mouse over it
+//     $(".popover").on("mouseenter", function(){
+//       $(this).show();
+//     });
+//     // popover hides when mouse out of it
+//     $(".popover").on("mouseleave", function(){
+//       $(this).hide();
+//     });
+//   }, function(){
+//     $(".popover").hide();
+//   });
 
 });
